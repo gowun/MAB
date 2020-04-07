3d792b6528d3f12be671b5dec935694baa7dc9e1
Gowun Jeong <kennie@Gowunui-MacBook-Pro.local>
2020-04-07 15:06:15 +0900

    tracking history of m

--- Mass_Simulation.py
+++ Mass_Simulation.py
@@ -51,14 +51,14 @@ class Mass_Simulation:
                         i_feedbacks.append(feedbacks[j])
                 rec_item_dist_per_model.append(self.compute_rec_item_distribution(i_items))
                 regrets_per_model.append(self.compute_regrets(i_users, i_items, i_feedbacks))
-            return rec_item_dist_per_model, regrets_per_model
-
+            return rec_item_dist_per_model, regrets_per_model, self.massMab.mab.M
         else:
-            return self.compute_rec_item_distribution(items), self.compute_regrets(self.users, items, feedbacks)
+            return self.compute_rec_item_distribution(items), self.compute_regrets(self.users, items, feedbacks), []
 
     def single_simulation(self):
         rec_item_dist = []
         regrets = []
+        hist_M = []
 
         for i in range(self.lenSim):
             ui_score_matrix_per_model = []
@@ -68,15 +68,17 @@ class Mass_Simulation:
                     ui_score_matrix_per_model.append(tmp)
                 if len(ui_score_matrix_per_model) == 1:
                     ui_score_matrix_per_model = ui_score_matrix_per_model[0]
-            dist, reg = self.one_step(ui_score_matrix_per_model)
+            dist, reg, mm = self.one_step(ui_score_matrix_per_model)
             if self.predModels is not None and len(self.predModels) > 1:
                 if i == 0:
                     rec_item_dist = [[d] for d in dist]
                     regrets = [[r] for r in reg]
+                    hist_M = [[m] for m in mm]
                 else:
                     for j, d in enumerate(dist):
                         rec_item_dist[j].append(d)
                         regrets[j].append(reg[j])
+                        hist_M[j].append(mm[j])
             else:
                 rec_item_dist.append(dist)
                 regrets.append(reg)
@@ -86,12 +88,13 @@ class Mass_Simulation:
         else:
             cum_regrets = np.cumsum(regrets)
 
-        return rec_item_dist, regrets, cum_regrets
+        return rec_item_dist, regrets, cum_regrets, hist_M
 
     def mean_simulation(self):
         list_rec_item_dist = []
         list_regrets = []
         list_cum_regrets = []
+        list_hist_M = []
 
         for i in range(self.nSim):
             tmp = self.single_simulation()
@@ -99,17 +102,21 @@ class Mass_Simulation:
             if self.predModels is not None and len(self.predModels) > 1:
                 if i == 0:
                     list_regrets = [[l] for l in tmp[1]]
+                    list_hist_M = [[m] for m in tmp[3]]
                 else:
                     for j, l in enumerate(tmp[1]):
                         list_regrets[j].append(l)
+                        list_hist_M[j].append(tmp[3][j])
             else:
                 list_regrets.append(tmp[1])
             list_cum_regrets.append(tmp[2])
         
         if self.predModels is not None and len(self.predModels) > 1:
             mean_regrets = [np.mean(r, axis=0) for r in list_regrets]
+            mean_hist_M = [np.mean(m, axis=0) for m in list_hist_M]
         else:
             mean_regrets = np.mean(list_regrets, axis=0)
+            mean_hist_M = []
         mean_cum_regrets = np.mean(list_cum_regrets, axis=0)
 
-        return mean_regrets, mean_cum_regrets, list_rec_item_dist
+        return mean_regrets, mean_cum_regrets, mean_hist_M, list_rec_item_dist
